5ce16eaab2be98a42a0cf1bf60ae9463c5812cff
facundoq <facundoq@gmail.com>
2022-03-28 11:59:46 -0300

    new version

--- rnutil/__init__.py
+++ rnutil/__init__.py
@@ -1,4 +1,5 @@
-print("redes-neuronales-util: Los archivos de los conjuntos de datos que ofrece este paquete también están disponibles en https://github.com/facundoq/redes-neuronales-util/tree/main/rnutil/data para descargar de forma individual")
+
+print(f"redes-neuronales-util: Los archivos de los conjuntos de datos que ofrece este paquete también están disponibles en https://github.com/facundoq/redes-neuronales-util/tree/main/rnutil/data para descargar de forma individual")
 print("redes-neuronales-util: Agregando el parámetro local=True en las funciones rnutil.load_dataset_numpy, rnutil.load_dataset_pandas y rnutil.load_image se puede cargar una versión local de un archivo en lugar de la versión que ofrece este paquete.")
 
 from .datasets import load_dataset_numpy,load_dataset_pandas,load_image
