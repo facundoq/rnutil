6a653f5d843b00db1a6d8f980c4666f7162f670f
facundoq <facundoq@gmail.com>
2022-03-28 18:02:51 -0300

    bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except FileNotFoundError:
     long_description = DESCRIPTION
 
 # Load the package's __version__.py module as a dictionary.
-VERSION = "0.3.2"
+VERSION = "0.3.3"
 
 
 class UploadCommand(Command):
