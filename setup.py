1d0151efdab5aad3c8760a5804821612be10145c
Facundo Quiroga <facundoq@users.noreply.github.com>
2022-08-22 07:28:04 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except FileNotFoundError:
     long_description = DESCRIPTION
 
 # Load the package's __version__.py module as a dictionary.
-VERSION = "0.4.4"
+VERSION = "0.4.5"
 
 
 class UploadCommand(Command):
